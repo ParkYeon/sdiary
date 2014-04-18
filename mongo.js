71c13e6f731ad4e950771308a44996e03f4c4c1f
ParkYeon <py111@naver.com>
2014-04-18 20:49:00 +0900

    Update mongo.js
    다른 샘플 사용

--- mongo.js
+++ mongo.js
@@ -1,34 +1,27 @@
-// MongoDB를 쓰기 위한 require
-var mongodb = require('mongodb');
-// 접속서버 세팅. auto_reconnect가 들어있는 3번째 파라메터는 옵션 모음
-var mongodb_server = new mongodb.Server('ds029950.mongolab.com', 29950, {auto_reconnect: true});
-// DB 세팅
-var db = mongodb.Db('sdiary', mongodb_server, {w:1});
+var mongodb = require('mongodb'),
+Db = mongodb.Db;
+ 
+var db = new Db('sdiary', new Server(process.env["ds029950.mongolab.com"], process.env["29950"], {}));
+ 
+db.open(function(err, db) {      
+    // 인증
+   db.authenticate(USERID, PASSWORD, function() {
+      if (err) { sys.debug("DB Authentication error!"); }
+                 
+      db.collection("test_collection", function(err, collection) {   
+      }); 
+   });
 
-// DB OPEN
-db.open(function(err, client){
-    if (err) {
-        console.log(err);
-        return;
-    }
-
-    // 인증 
-    // 옵션인 3번째 파라메터의 authSource: admin 이 포인트...ㅠ_ㅠ
-    db.authenticate('test_app', 'test_pass', {'authSource': 'admin'}, function(err){
-        if (err) {
-            console.log(err);
-            return;
-        }
-
-        // collection 선택
-        var col = new mongodb.Collection(client, 'mynewcollection');
-        // 선택한 collection 전체 item iteration
-        col.find().toArray(function(err, item){
-            if (err) {
-                console.log(err);
-                return;   
+   db.collection("mynewcollection", function(err, collection) {
+      collection.find({"cmd":cmd}, {"sort":"order"}, function(err, cursor) {
+         cursor.each(function(err, item) {
+            if (item != null) {
+               // Do something
             }
-            console.log(item);
-        });
-    });
+                
+            if (item != null) {} // 반복문 끝
+         });
+         db.close();
+      });   
+   }); 
 });
